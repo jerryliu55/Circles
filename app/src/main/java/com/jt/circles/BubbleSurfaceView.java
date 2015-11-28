12afbdb7407d109c50e5e1c7dfe3f0dbc036396c
jerryliu55 <jerryliu55@gmail.com>
2015-11-28 06:45:46 -0500

    the thing moves due to accelerometer

--- app/src/main/java/com/jt/circles/BubbleSurfaceView.java
+++ app/src/main/java/com/jt/circles/BubbleSurfaceView.java
@@ -16,6 +16,8 @@ public class BubbleSurfaceView extends SurfaceView implements SurfaceHolder.Call
     private SurfaceHolder sh;
     private BubbleThread thread;
     private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final Paint paintSprite = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static float acx = 0, acy = 0;
 
     public BubbleSurfaceView(Context context) {
         super(context);
@@ -23,6 +25,8 @@ public class BubbleSurfaceView extends SurfaceView implements SurfaceHolder.Call
         sh.addCallback(this);
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
+        paintSprite.setColor(Color.GREEN);
+        paintSprite.setStyle(Paint.Style.FILL);
         ctx = context;
         setFocusable(true);
     }
@@ -50,6 +54,12 @@ public class BubbleSurfaceView extends SurfaceView implements SurfaceHolder.Call
         }
     }
 
+    public static void setAccel(float x, float y)
+    {
+        acx = x;
+        acy = y;
+    }
+
     public BubbleThread getThread()
     {
         return thread;
@@ -59,12 +69,17 @@ public class BubbleSurfaceView extends SurfaceView implements SurfaceHolder.Call
         private int canvasWidth = 200;
         private int canvasHeight = 400;
         private static final int SPEED = 8;
+        private static final int SPRITE_SPEED = 4;
+        private static final float MAX_SPEED = 4;
         private boolean run = false;
 
         private float bubbleX;
         private float bubbleY;
         private float headingX;
         private float headingY;
+
+        private float spriteX;
+        private float spriteY;
 
         public BubbleThread(SurfaceHolder surfaceHolder, Context context,
                             Handler handler) {
@@ -77,6 +92,8 @@ public class BubbleSurfaceView extends SurfaceView implements SurfaceHolder.Call
                 // Start bubble in centre and create some random motion
                 bubbleX = canvasWidth / 2;
                 bubbleY = canvasHeight / 2;
+                spriteX = canvasWidth / 2;
+                spriteY = canvasHeight / 2;
                 headingX = (float) (-1 + (Math.random() * 2));
                 headingY = (float) (-1 + (Math.random() * 2));
             }
@@ -112,11 +129,36 @@ public class BubbleSurfaceView extends SurfaceView implements SurfaceHolder.Call
             if (bubbleY <= 50 || bubbleY >= canvasHeight - 50) headingY *= -1;
             bubbleX = bubbleX + (headingX * SPEED);
             bubbleY = bubbleY + (headingY * SPEED);
-            canvas.save();
-            canvas.restore();
+            // move the sprite
+            // limit total speed to 2
+            float a = 1;
+            if (Math.sqrt(acx * acx + acy * acy) > MAX_SPEED)
+            {
+                a = (float) (MAX_SPEED / Math.sqrt(acx * acx + acy * acy));
+            }
+            // x and y accel
+            acx = a * acx;
+            acy = a * acy;
+            if (Math.abs(acx) > 0.7)
+            {
+                //if (Math.abs(acx) > 2) acx = 2 * acx/Math.abs(acx);
+                spriteX += SPRITE_SPEED * acx;
+            }
+            if (Math.abs(acy) > 0.7)
+            {
+                //if (Math.abs(acy) > 2) acy = 2 * acy/Math.abs(acy);
+                spriteY += SPRITE_SPEED * acy;
+            }
+            if (spriteX <= 50) spriteX = 50;
+            if (spriteX >= canvasWidth - 50) spriteX = canvasWidth - 50;
+            if (spriteY <= 50) spriteY = 50;
+            if (spriteY >= canvasHeight - 50) spriteY = canvasHeight - 50;
+
+            /*canvas.save();
+            canvas.restore();*/
             canvas.drawColor(Color.WHITE);
             canvas.drawCircle(bubbleX, bubbleY, 50, paint);
-            canvas.drawCircle(600, 500, 50, paint);
+            canvas.drawCircle(spriteX, spriteY, 50, paint);
         }
     }
 }
--- app/src/main/java/com/jt/circles/GameActivity.java
+++ app/src/main/java/com/jt/circles/GameActivity.java
@@ -1,19 +1,60 @@
 package com.jt.circles;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
+    private float x, y, z;
+    private float ix, iy, iz;
+    private TextView tv;
+    private boolean ini = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(new BubbleSurfaceView(this));
+        //setContentView(R.layout.activity_game);
+        tv = (TextView) findViewById(R.id.display);
+
+        //listener for accelerometer, use anonymous class for simplicity
+        ((SensorManager)getSystemService(Context.SENSOR_SERVICE)).registerListener(
+            new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    //set ball speed based on phone tilt (ignore Z axis)
+                    if (ini)
+                    {
+                        ix = -event.values[0];
+                        iy = event.values[1];
+                        iz = event.values[2];
+                        ini = false;
+                    }
+                    x = -event.values[0] - ix;
+                    y = event.values[1] - iy;
+                    z = event.values[2] - iz;
+                    //timer event will redraw ball
+                    BubbleSurfaceView.setAccel(x, y);
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                } //ignore
+            },
+            ((SensorManager) getSystemService(Context.SENSOR_SERVICE))
+                .getSensorList(Sensor.TYPE_ACCELEROMETER).get(0),
+                SensorManager.SENSOR_DELAY_GAME);
     }
 
     /*@Override
